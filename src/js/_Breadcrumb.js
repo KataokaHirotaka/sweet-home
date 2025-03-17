35322a54f3a585066a940e65f3bf34ec944d6648
片岡潤隆 <>
2025-03-17 23:30:53 +0900

    リンク修正

--- src/js/_Breadcrumb.js
+++ src/js/_Breadcrumb.js
@@ -22,32 +22,32 @@ export default class Breadcrumb {
         <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/news" class="visited">新着情報</a></li>
         <li class="l-breadcrumbList_item" id="js-breadcrumbTxt"></li>
       `;
-    } else if (this.path === "/info") {
+    } else if (this.path === "/info/") {
       breadcrumbHTML += `
           <li class="l-breadcrumbList_item visited"><a href="/" class="icon"></a></li>
           <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;ご案内</li>
       `;
-    } else if (this.path === "/info/price") {
+    } else if (this.path === "/info/price/") {
       breadcrumbHTML += `
         <li class="l-breadcrumbList_item visited"><a href="/" class="icon"></a></li>
         <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/info" class="visited">ご案内</a></li>
         <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;ご利用料金</li>
       `;
-    } else if (this.path === "/info/price/month") {
+    } else if (this.path === "/info/price/month/") {
       breadcrumbHTML += `
         <li class="l-breadcrumbList_item visited"><a href="/" class="icon"></a></li>
         <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/info" class="visited">ご案内</a></li>
         <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/info/price" class="visited">ご利用料金</a></li>
         <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;月額料金案内</li>
       `;
-    } else if (this.path === "/info/price/temporary") {
+    } else if (this.path === "/info/price/temporary/") {
       breadcrumbHTML += `
       <li class="l-breadcrumbList_item visited"><a href="/" class="icon"></a></li>
       <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/info" class="visited">ご案内</a></li>
       <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/info/price" class="visited">ご利用料金</a></li>
       <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;一時預かり料金案内</li>
     `;
-    } else if (this.path === "/info/price/other") {
+    } else if (this.path === "/info/price/other/") {
       breadcrumbHTML += `
       <li class="l-breadcrumbList_item visited"><a href="/" class="icon"></a></li>
       <li class="l-breadcrumbList_item">&nbsp;&gt;&nbsp;<a href="/info" class="visited">ご案内</a></li>
